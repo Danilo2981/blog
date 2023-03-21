0473f442b70c4eaa1f7f49b308f95fcef77da87e
Danilo <danilo.vega@hotmail.es>
2023-03-21 16:53:31 -0500

    Admin Panel

--- resources/views/layouts/partials/admin/navigation.blade.php
+++ resources/views/layouts/partials/admin/navigation.blade.php
@@ -1,10 +1,10 @@
-<nav class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 sticky top-0">
+<nav class="bg-indigo-300 dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 sticky top-0">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <!-- Hamburger -->
             <div class="-mr-2 items-center hidden sm:flex">
-                <button @click="openSidebar = ! openSidebar" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+                <button @click="openSidebar = ! openSidebar" class="inline-flex items-center justify-center p-2 rounded-md text-gray-500 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
--- resources/views/layouts/partials/admin/sidebar.blade.php
+++ resources/views/layouts/partials/admin/sidebar.blade.php
@@ -1,38 +1,41 @@
-<aside class="w-64 sticky top-0 z-10 shadow">
-    <div class="px-3 py-4 h-screen overflow-y-auto bg-gray-50 dark:bg-gray-800">
-        <div class="sticky top-0 z-10 bg-white dark:bg-gray-800 px-4 py-1">
-            <a href="/" class="flex items-center pl-2.5 mb-5">
-                <x-application-mark class="h-6 mr-3 sm:h-7" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Del putas</span>
-            </a>
-        </div>
-        <div class="flex flex-col justify-between">
-            <ul class="space-y-2 mb-12">
-                @foreach ($links as $link)
-                <li>
-                    <a href="{{ $link['url'] }}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 {{ $link['active'] ? 'bg-gray-200 dark:bg-gray-500' : '' }}">
-                        <span class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
-                            <i class="{{ $link['icon'] }}"></i>
-                        </span>
-                        <span class="ml-3">{{ $link['title'] }}</span>
-                    </a>
-                </li>
-                @endforeach
-            </ul>
-            <ul class="pt-4 mt-12  space-y-2 border-t border-gray-200 dark:border-gray-700">
-                <li>
-                    <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
-                        <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white dark:text-gray-400" focusable="false" data-prefix="fas" data-icon="gem" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M378.7 32H133.3L256 182.7L378.7 32zM512 192l-107.4-141.3L289.6 192H512zM107.4 50.67L0 192h222.4L107.4 50.67zM244.3 474.9C247.3 478.2 251.6 480 256 480s8.653-1.828 11.67-5.062L510.6 224H1.365L244.3 474.9z"></path></svg>
-                        <span class="ml-4">Upgrade to Pro</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group">
-                        <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z" clip-rule="evenodd"></path></svg>
-                        <span class="ml-3">Help</span>
-                    </a>
-                </li>
-            </ul>
+<aside class="w-64 sticky top-0 z-10 bg-indigo-300 dark:bg-gray-800">
+    <div class="px-2 py-2 h-screen">
+        <div class="flex flex-col h-full justify-between px-3 py-4 bg-indigo-50 dark:bg-gray-600 rounded-md">
+            <div class="top-0 z-10 px-4 py-2 mb-4 bg-indigo-50 rounded-md">
+                <a href="/" class="flex items-center pl-2.5 mb-1">
+                    <x-application-mark class="h-6 mr-3 sm:h-7" />
+                </a>
+            </div>
+            <div class="flex flex-col justify-between h-full pr-2 mr-0 overflow-y-auto">
+                <ul class="space-y-2 mb-4">
+                    @foreach ($links as $link)
+                    <li>
+                        <a href="{{ $link['url'] }}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-gray-50 hover:bg-indigo-300 dark:hover:bg-gray-700 {{ $link['active'] ? 'bg-gray-300 dark:bg-gray-500' : '' }}">
+                            <span class="w-6 h-6 text-gray-800 transition duration-75 dark:text-gray-100 group-hover:text-gray-900 dark:group-hover:text-indigo-300">
+                                <i class="{{ $link['icon'] }}"></i>
+                            </span>
+                            <span class="ml-3">{{ $link['title'] }}</span>
+                        </a>
+                    </li>
+                    @endforeach
+                </ul> 
+            </div>
+            <div class="flex flex-col justify-between">
+                <ul class="pt-4 mt-4 space-y-2 border-t border-gray-200 dark:border-gray-700">
+                    <li>
+                        <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg hover:bg-indigo-300 dark:hover:bg-gray-700 dark:text-gray-100 group">
+                            <i class="fa-solid fa-gem"></i>
+                            <span class="ml-4">Upgrade to Pro</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg hover:bg-indigo-300 dark:hover:bg-gray-700 dark:text-gray-100 group">
+                            <i class="fa-solid fa-circle-question"></i>
+                            <span class="ml-3">Help</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </aside>
